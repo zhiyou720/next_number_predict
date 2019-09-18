8b4acb8da662f265d7719fc5ba0c25fad5856754
Your Name <you@example.com>
2019-09-18 14:54:52 +0800

    change time point feature style

--- data_helper.py
+++ data_helper.py
@@ -98,13 +98,13 @@ class DataLoader:
         features = self.build_features()
         labels = self.build_labels()
 
-        while ptr + self.seq_len < len(self.origin_data_set.year):
+        while ptr + self.seq_len < len(self.origin_data_set.year[:200]):
             delta = 0
             tmp_x = []
             tmp_y = [0 for x in range(self.class_num)]
             p = 1
             while p <= self.seq_len:
-                stack = features[ptr + delta] + list(labels[ptr + delta])
+                stack = features[ptr + delta][:-1] + [features[ptr + delta][-1] + labels[ptr + delta]]
                 tmp_x += stack
                 delta += 1
                 p += 1
